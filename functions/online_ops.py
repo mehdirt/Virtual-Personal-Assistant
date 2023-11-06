6fba464fbd05fc6cb6a3daa08ec0128521bf296e
Mahdi Rafati <mahdirafati680@gmail.com>
2023-11-06 18:19:44 +0330

    The sending email function added

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -5,6 +5,9 @@ from email.message import EmailMessage
 import smtplib
 from decouple import config
 
+EMAIL = config('EMAIL')
+PASSWORD = config('PASSWORD')
+
 def find_my_ip():
     ip_address = requests.get("https://api64.ipify.org/?format=json").json()
     return ip_address['ip']
@@ -21,4 +24,21 @@ def search_on_google(query):
 
 def send_whatsapp_message(number, message):
     kit.sendwhatmsg_instantly(f"+98{number}", message)
-    
+
+def send_email(receiver_address, subject, message):
+    try:
+        email = EmailMessage()
+        email['TO'] = receiver_address
+        email['Subject'] = subject
+        email['From'] = EMAIL
+        email.set_content(message)
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.send_message(email)
+        s.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
