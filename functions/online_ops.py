6fe4345779d1036e18190ddb608b91a20e59dfa3
Mahdi Rafati <mahdirafati680@gmail.com>
2023-11-07 00:45:33 +0330

    Three online functions added (get_news, get_movies, get_weather )

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -5,8 +5,12 @@ from email.message import EmailMessage
 import smtplib
 from decouple import config
 
+# Config enviroment variables
 EMAIL = config('EMAIL')
 PASSWORD = config('PASSWORD')
+NEWS_API_KEY = config('NEWS_API_KEY')
+WEATHER_API_KEY = config("WEATHER_API_KEY")
+TMDB_API_KEY = config('TMDB_API_KEY')
 
 def find_my_ip():
     ip_address = requests.get("https://api64.ipify.org/?format=json").json()
@@ -42,3 +46,28 @@ def send_email(receiver_address, subject, message):
         print(e)
         return False
 
+def get_latest_news():
+    news_headlines = []
+    res = requests.get(f"https://newsapi.org/v2/top-headlines?
+                       country=us&apiKey={NEWS_API_KEY}&category=general").json()
+    articles = res['articles']
+    for article in articles:
+        news_headlines.append(article['title'])
+    return news_headlines[:5]
+
+def get_weather_app(city): #TODO: test needed!
+    res = requests.get(f"http://api.weatherapi.com/v1/current.json?
+                      key={WEATHER_API_KEY}&q={city}").json()
+    weather = res['condition:text']
+    temperature = res['temp_c']
+    feels_like = res['feelslike_c']
+    return (weather, f"{temperature}℃", f"{feels_like}℃")
+
+def get_trending_movies():
+    trending_movies = []
+    res = requests.get(
+        f"https://api.themoviedb.org/3/trending/movie/day?api_key={TMDB_API_KEY}").json()
+    results = res["results"]
+    for r in results:
+        trending_movies.append(r["original_title"])
+    return trending_movies[:5]
