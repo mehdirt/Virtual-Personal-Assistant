950bb691a22279cbc8e02b3a5222816a63b16b0c
Mahdi Rafati <mahdirafati680@gmail.com>
2023-11-21 19:27:00 +0330

    __init__.py added

---
+++ functions/__init__.py
@@ -0,0 +1,3 @@
+from os_ops import open_camera, open_notepad, open_cmd, open_calculator, open_discord
+from online_ops import find_my_ip, search_on_google, search_on_wikipedia, send_email, \
+send_whatsapp_message, get_latest_news, get_random_advice, get_random_joke, get_trending_movies, get_weather_report, play_on_youtube
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from random import choice
 from utils import opening_text
 import requests
 from pprint import pprint
-from functions.os_ops import open_camera, open_notepad, open_cmd, open_calculator, open_discord
-from functions.online_ops import find_my_ip, search_on_google, search_on_wikipedia, send_email, \
+from functions import open_camera, open_notepad, open_cmd, open_calculator, open_discord, \
+find_my_ip, search_on_google, search_on_wikipedia, send_email, \
 send_whatsapp_message, get_latest_news, get_random_advice, get_random_joke, get_trending_movies, get_weather_report, play_on_youtube
 
 # Config enviroment variables
